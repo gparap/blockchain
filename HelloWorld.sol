3e2cabbb25fec918b4846f136355d70d28a0f94b
gparap <gparap@protonmail.com>
2021-09-26 18:43:33 +0200

    Initial commit

---
+++ HelloWorld.sol
@@ -0,0 +1,22 @@
+/*Copyright © 2021 gparap*/
+pragma solidity ^0.4.0;
+
+contract HelloWorld{
+    string value;
+    
+    //initialize value
+    constructor() public {
+        value = "hello world!";
+    }
+    
+    //get my value
+    function getValue() public view
+    returns(string) {
+        return value;
+    }
+    
+    //set my value to a new value
+    function setValue(string newValue) public {
+        value = newValue;
+    }
+}
