31a3b953d74ed05b8bb495629d955c45c03d83b6
gparap <gparap@protonmail.com>
2021-12-19 21:58:09 +0200

    get data from the web server asynchronously

--- EthereumTestRPC/app.js
+++ EthereumTestRPC/app.js
@@ -1,11 +1,21 @@
 // create an Express application
 var express = require('express');
 var app = express();
+
 var port = 9999;
+
+// create a response object to send
+var responseObj = {key: "response object value"};
 
 // create a basic route 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
+});
+
+// create a route for pinging the web server
+app.get('/ping', (request, response) => {
+    // response.send("Pinging port " + port);
+    response.send(JSON.stringify(responseObj));
 });
 
 // listen for connections
