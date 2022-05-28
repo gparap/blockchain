cbbd3da2bb0a97e6b459cdeb1cb9b56a3f4503ce
gparap <gparap@protonmail.com>
2022-05-28 16:18:14 +0200

    Add contract owner and pay the winner

--- smart-contracts/Lotto.sol
+++ smart-contracts/Lotto.sol
@@ -10,24 +10,38 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Lotto {
     uint256 pool;
     mapping(address => uint256) public gambles;
-    mapping(address => uint256) public prizes;
     uint256 gamblesCount;
+    address payable private owner;
+    address payable[] public gamblers;
 
     constructor() {
         //initialize
         pool = 0;
         gamblesCount = 0;
+        owner = payable(msg.sender);
+    }
+
+    modifier isOwner() {
+        //check if the caller is the owner
+        require(msg.sender == owner, "This caller is not the contract owner...");
+        _;
+    }
+
+    //get contract owner
+    function getOwner() public view returns(address) {
+        return owner;
     }
 
     //add ether to the lottery pool
-    function gambleAmount(uint256 amount) public {
-        //add person amount to list
-        gambles[msg.sender] += amount;
+    function gambleAmount() public payable {
+
+        //add person address & amount to lists
+        gamblers.push(payable(msg.sender));
+        gambles[msg.sender] += msg.value;
         gamblesCount += 1;
-        prizes[msg.sender] = 0;
 
         //add ether to pool
-        pool += amount;
+        pool += msg.value;
     }
 
     //get the total amount of ether in the pool
@@ -40,17 +54,26 @@ contract Lotto {
         return gambles[msg.sender];
     }
 
-    //display the amount that the person has won in the lottery
-    function getPersonPrizeAmount() public view returns(uint256) {
-        return prizes[msg.sender];
-    }
-
-    function performTheLottery() public view returns(uint256) {
+    function performTheLottery() private view returns(uint256) {
         //randomize
         uint256 random = uint256(keccak256("gparap"));
         uint256 winnerIndex = random % gamblesCount;
 
         //return the index of the person who won the lottery
         return winnerIndex;
-  }
+    }
+
+    function payTheWinner() isOwner public {
+        uint256 winnerIndex = performTheLottery();
+        address payable winningAddress;
+        uint256 i;
+
+        //loop through the gamblers to find and pay the winner
+        for (i = 0; i < gamblesCount; i++) {
+            if (i == winnerIndex) {
+                winningAddress = gamblers[i];
+                winningAddress.transfer(address(this).balance);
+            }
+        }
+    }
 }
